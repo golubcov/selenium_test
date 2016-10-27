1d581da6f7e47cd23a67368336dbe26d06cedfe8
Eugene <golubcov.e@gmail.com>
2016-10-27 17:29:58 +0300

    js

---
+++ test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Eugene on 27.10.16.
+ */
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
+
